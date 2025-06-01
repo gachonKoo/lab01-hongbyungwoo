36bed8cf4846b188a6e19b37289e205b7af7b97c
JaKeoung Koo <jakeoung@users.noreply.github.com>
2025-06-01 11:43:39 +0900

    Update a_test.py

--- a_test.py
+++ a_test.py
@@ -1,7 +1,15 @@
 from divisors import divide
+import csv
 
 def func(x):
     return x + 2
 
 def test_answer():
+    with open('eggs.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+
     assert divide(4) == 3
+    
+    
